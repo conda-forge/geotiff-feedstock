ee9d5abff44957e52ffa5416eeb3e5862feaad48
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-07-20 01:09:23 +0200

    Fix deactivate script

--- recipe/scripts/deactivate.sh
+++ recipe/scripts/deactivate.sh
@@ -2,7 +2,7 @@
 # Restore previous GeoTIFF env vars if they were set
 
 unset GEOTIFF_CSV
-if [ -n "${_CONDA_SET_GEOTIFF_CSV:-}" ]]; then
+if [ -n "${_CONDA_SET_GEOTIFF_CSV:-}" ]; then
     export GEOTIFF_CSV="$_CONDA_SET_GEOTIFF_CSV"
     unset _CONDA_SET_GEOTIFF_CSV
 fi
